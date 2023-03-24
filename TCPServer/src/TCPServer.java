f204b9d3e626f562981462e92c58e175ceead403
Bojechko <34715286+Bojechko@users.noreply.github.com>
2023-03-24 22:02:54 +0300

    removed trash

--- TCPServer/src/TCPServer.java
+++ TCPServer/src/TCPServer.java
@@ -31,8 +31,6 @@ public class TCPServer {
                     words = reader.readLine().split(" ");
 
                     if (words[0].equals("GUESS")){
-                        System.out.println(words[0] );
-                        System.out.println(words[1] );
                         if (Integer.parseInt(words[1]) == randomNum) {
                             writer.println("Server: " + "EQUAL");
 
