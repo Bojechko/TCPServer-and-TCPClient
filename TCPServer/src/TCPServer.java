2fefbfe5dc7f52e9794c7af78e7a649999a2fa6c
Bojechko <34715286+Bojechko@users.noreply.github.com>
2023-03-24 21:34:49 +0300

    done

---
+++ TCPServer/src/TCPServer.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TCPServer {
+
+    public static void main(String[] args) {
+        if (args.length < 1) return;
+
+        int port = Integer.parseInt(args[0]);
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Server is listening on port " + port);
+
+            int randomNum = ThreadLocalRandom.current().nextInt(0, 10);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+                System.out.println(String.valueOf(randomNum));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                String[] words;
+
+                do {
+                    InputStream input = socket.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    words = reader.readLine().split(" ");
+
+                    if (Integer.parseInt(words[1]) == randomNum) {
+                        writer.println("Server: " + "EQUAL");
+
+                        randomNum = ThreadLocalRandom.current().nextInt(0, 10);
+                        System.out.println(String.valueOf(randomNum));
+                    }
+
+                    if (Integer.parseInt(words[1]) > randomNum) {
+                        writer.println("Server: " + "MORE");
+                    }
+
+                    if (Integer.parseInt(words[1]) < randomNum) {
+                        writer.println("Server: " + "LESS");
+                    }
+                } while (!words[0].equals("bye"));
+
+                socket.close();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
